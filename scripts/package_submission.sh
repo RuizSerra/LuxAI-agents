82c3c9532f3167fdc158fc013c896aa8d25ad40f
Jaime <jaimeruizno@gmail.com>
2021-09-12 13:18:01 +1000

    dump

--- scripts/package_submission.sh
+++ scripts/package_submission.sh
@@ -6,14 +6,16 @@ set -e
 
 WORKING_DIR="/Users/jaime/Downloads"
 
-echo "Working dir: " $WORKING_DIR
-
 DATE=$(date '+%Y%m%d-%H%M%S')
 TARGET_DIR_NAME="lux-${DATE}"
 
-echo "Target dir: " "${WORKING_DIR}/${TARGET_DIR_NAME}"
-
 mkdir "${WORKING_DIR}/${TARGET_DIR_NAME}"
-cp -r agents agent_loader.py main.py "${WORKING_DIR}/${TARGET_DIR_NAME}"
+cp -r agents lux __init__.py agent_loader.py main.py "${WORKING_DIR}/${TARGET_DIR_NAME}"
 cd $WORKING_DIR
 tar -czf submission.tar.gz $TARGET_DIR_NAME
+
+echo
+echo "Packaged. You may submit with:"
+echo "kaggle competitions submit -c lux-ai-2021 -f $WORKING_DIR/submission.tar.gz -m 'Message'"
+echo
+echo "Consider deleting ${WORKING_DIR}/${TARGET_DIR_NAME}"
